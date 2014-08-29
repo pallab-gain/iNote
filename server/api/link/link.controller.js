046cacd0cdf5dc11d196fd3fe5ceca2acb7bcb0c
pallab-gain <pallab.gain@gmail.com>
2014-08-30 00:18:54 +0600

    create new url

--- server/api/link/link.controller.js
+++ server/api/link/link.controller.js
@@ -4,56 +4,86 @@ var _ = require('lodash');
 var Link = require('./link.model');
 
 // Get list of links
-exports.index = function(req, res) {
-  Link.find(function (err, links) {
-    if(err) { return handleError(res, err); }
-    return res.json(200, links);
-  });
+exports.index = function (req, res) {
+    Link.find(function (err, links) {
+        if (err) {
+            return handleError(res, err);
+        }
+        return res.json(200, links);
+    });
 };
 
 // Get a single link
-exports.show = function(req, res) {
-  Link.findById(req.params.id, function (err, link) {
-    if(err) { return handleError(res, err); }
-    if(!link) { return res.send(404); }
-    return res.json(link);
-  });
+exports.show = function (req, res) {
+
+    Link.findById(req.params.id, function (err, link) {
+        if (err) {
+            return handleError(res, err);
+        }
+        if (!link) {
+            return res.send(404);
+        }
+        return res.json(link);
+    });
 };
 
 // Creates a new link in the DB.
-exports.create = function(req, res) {
-  Link.create(req.body, function(err, link) {
-    if(err) { return handleError(res, err); }
-    return res.json(201, link);
-  });
+exports.create = function (req, res) {
+    var link = undefined;
+    if (!_.isUndefined(req.body.url)) {
+        link = {url: req.body.url};
+        Link.create(link, function (err, link) {
+            if (err) {
+                return handleError(res, err);
+            }
+            return res.json(201, link);
+        });
+    } else {
+        return handleError(res, 'Provide a link url');
+    }
+
 };
 
 // Updates an existing link in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  Link.findById(req.params.id, function (err, link) {
-    if (err) { return handleError(res, err); }
-    if(!link) { return res.send(404); }
-    var updated = _.merge(link, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.json(200, link);
+exports.update = function (req, res) {
+    if (req.body._id) {
+        delete req.body._id;
+    }
+    Link.findById(req.params.id, function (err, link) {
+        if (err) {
+            return handleError(res, err);
+        }
+        if (!link) {
+            return res.send(404);
+        }
+        var updated = _.merge(link, req.body);
+        updated.save(function (err) {
+            if (err) {
+                return handleError(res, err);
+            }
+            return res.json(200, link);
+        });
     });
-  });
 };
 
 // Deletes a link from the DB.
-exports.destroy = function(req, res) {
-  Link.findById(req.params.id, function (err, link) {
-    if(err) { return handleError(res, err); }
-    if(!link) { return res.send(404); }
-    link.remove(function(err) {
-      if(err) { return handleError(res, err); }
-      return res.send(204);
+exports.destroy = function (req, res) {
+    Link.findById(req.params.id, function (err, link) {
+        if (err) {
+            return handleError(res, err);
+        }
+        if (!link) {
+            return res.send(404);
+        }
+        link.remove(function (err) {
+            if (err) {
+                return handleError(res, err);
+            }
+            return res.send(204);
+        });
     });
-  });
 };
 
 function handleError(res, err) {
-  return res.send(500, err);
+    return res.send(500, err);
 }
